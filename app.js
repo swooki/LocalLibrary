d48a7ea9efaa8d87f4f3cdd84093b43988318319
Sungwook Kwon <swooki@gmail.com>
2020-03-19 14:46:28 -0400

    finished part-4

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
 
 var app = express();
 
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog',catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
